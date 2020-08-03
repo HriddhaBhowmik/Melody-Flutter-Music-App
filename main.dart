cc4270d1a57940f866b3dcf5345146fc92226177
HriddhaBhowmik <68870087+HriddhaBhowmik@users.noreply.github.com>
2020-08-03 20:44:41 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:task1_app/ui/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return myapp();
+  }
+}
